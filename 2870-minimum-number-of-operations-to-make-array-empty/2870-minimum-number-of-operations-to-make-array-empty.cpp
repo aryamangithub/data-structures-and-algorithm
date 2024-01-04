ee5f9b6f88b98e9d870276f4f64fe0fa4b58afef
Aryaman Singh Chandrawat <singharyaman357@gmail.com>
2024-01-05 01:42:50 +0530

    Time: 122 ms (56.03%), Space: 81.2 MB (96.66%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        int start = 0;
+        
+        while(start < nums.size()){
+            int end = start;
+            while(end < nums.size() && nums[start] == nums[end])
+            {
+                end++;
+            }
+            
+            int count = end - start;
+            if(count == 1) return -1;
+            
+            res += count/3;
+            if(count%3 != 0) res++;
+            
+            start = end;
+        }
+        return res;
+    }
+};
